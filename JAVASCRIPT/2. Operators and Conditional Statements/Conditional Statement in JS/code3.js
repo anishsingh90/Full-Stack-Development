8ca265127ba13bfc6a90839ba6ac23cb82c7c303
ANISH-SINGH <anishraaz90@gmail.com>
2024-11-22 23:07:00 +0530

    practice

---
+++ JAVASCRIPT/2. Operators and Conditional Statements/Conditional Statement in JS/code3.js
@@ -0,0 +1,6 @@
+//Ternary Operator
+let age = 2;
+
+let result = age >= 18 ? "adult" : "not adult";
+
+console.log(result);
---
+++ JAVASCRIPT/2. Operators and Conditional Statements/Conditional Statement in JS/code4.js
@@ -0,0 +1,25 @@
+/* Qs2. Write a code which can give grades to students according to their scores.
+ 80-100, A
+ 70-89, B
+ 60-69, C
+ 50-59, D
+ 0-49, F
+*/
+
+let marks = prompt("Enter the marks");
+
+//Ensures marks is converted to a number
+
+if(marks >= 80 && marks <= 100){
+    console.log("Grade: A");
+} else if(marks >= 70 && marks <= 79){
+    console.log("Grade: B");
+} else if(marks >= 60 && marks <= 69){
+    console.log("Grade: C");
+} else if(marks >= 50 && marks <= 59){
+    console.log("Grade: D");
+} else if(marks >= 0 && marks <= 49){
+    console.log("Grade: F");
+} else{
+    console.log("Invalid marks entered");
+}
