4e477df9dec677b19c7109637e90551f684217f7
ANISH-SINGH <anishraaz90@gmail.com>
2024-12-01 01:50:33 +0530

    today code

--- REACT.js/my-app/src/App.js
+++ REACT.js/my-app/src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="blank">
       Anish Singh
+      <div className='git'>
+        <a href="https://github.com/anishsingh">GitHub</a>
+      </div>
     </div>
   );
 }
