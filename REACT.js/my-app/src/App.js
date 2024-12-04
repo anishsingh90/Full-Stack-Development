3508be6bb7641e93da929cac3777c63f7b4cbf8e
ANISH-SINGH <anishraaz90@gmail.com>
2024-12-05 00:37:03 +0530

    use state

--- REACT.js/my-app/src/App.js
+++ REACT.js/my-app/src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
-import Navbar from './Components/props & types/Navbar';
-import TextForm from './Components/UseState & state/TextForm';
+import "./App.css";
+import Navbar from "./Components/6. props & types/Navbar";
+import About from "./Components/7. UseState & state/About";
+// import TextForm from './Components/7. UseState & state/TextForm'
 
 function App() {
   return (
     <>
-   
-   <Navbar title="TextUtils" aboutText="About TextUtils"/>
-   {/* <Navbar/> */}
-  
-  <div className="container my-3">
-    <TextForm heading="Enter the text to analyze"/>
-  </div>
+      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar />
+
+      <div className="container my-3">
+        {/* <TextForm heading="Enter the text to analyze"/> */}
+        {/* <TextForm/> */}
+        <About />
+      </div>
     </>
   );
 }
--- REACT.js/my-app/src/Components/7. UseState & state/TextForm.js
+++ REACT.js/my-app/src/Components/7. UseState & state/TextForm.js
@@ -1,51 +1,67 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const handleUpClick = () =>{
-        // console.log("Uppercase was clicked" + text);
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
-
-    const handleLowClick = () =>{
-      // console.log("Lowercase was clicked" + text);
-      let newText = text.toLowerCase();
-      setText(newText);
-  } 
-  
-  const handleClearClick = ()=>{
-    let newText = '';
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked" + text);
+    let newText = text.toUpperCase();
     setText(newText);
-  }
+  };
 
-    const handleOnChange = (event) =>{
-        // console.log("On Changed");
-        setText(event.target.value);
-    }
+  const handleLowClick = () => {
+    // console.log("Lowercase was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
 
-    const [text, setText] = useState('');
-    // text = "new text" //Wrong way to change the state
-    // setText("new text"); //Correct way to change the state
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    // console.log("On Changed");
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("");
+  // text = "new text" //Wrong way to change the state
+  // setText("new text"); //Correct way to change the state
 
   return (
     <>
-    <div className='container'>
+      <div className="container">
         <h1>{props.heading}</h1>
-  <div className="mb-3">
-    <label for="myBox" className="form-label"></label>
-    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"> </textarea>
-  </div>
-  <button className="btn btn-primary " onClick={handleUpClick}>Convert to UpperCase</button>
-  <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
-  <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-    </div>
-    <div className="container my-2">
-      <h2>Your text summary</h2>
-      <p>{text.split(" ").length} words and {text.length} characeters</p>
-      <p>{0.008 * text.split(" ").length} Minutes read</p>
-      <h2>Preview</h2>
-      <p>{text}</p>
-    </div>
+        <div className="mb-3">
+          <label for="myBox" className="form-label"></label>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          >
+            {" "}
+          </textarea>
+        </div>
+        <button className="btn btn-primary " onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Clear Text
+        </button>
+      </div>
+      <div className="container my-2">
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characeters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
     </>
   );
 }
