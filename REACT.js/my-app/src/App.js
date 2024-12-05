2a2c83eb5ccb411f4c2f5bc1bce795978af8b8f7
ANISH-SINGH <anishraaz90@gmail.com>
2024-12-06 00:07:19 +0530

    modified

--- REACT.js/my-app/src/App.js
+++ REACT.js/my-app/src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
 import Navbar from "./Components/6. props & types/Navbar";
-import About from "./Components/7. UseState & state/About";
-// import TextForm from './Components/7. UseState & state/TextForm'
+// import About from "./Components/7. UseState & state/About";
+import TextForm from "./Components/7. UseState & state/TextForm";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light"); //Whether dark mode is enable or not
+
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
-      <Navbar />
+      {/* Navbar.js access from Here  */}
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar /> */}
+      <Navbar title="TextUtils" mode={mode} />
 
+      {/* TextForm.js access from Here  */}
       <div className="container my-3">
-        {/* <TextForm heading="Enter the text to analyze"/> */}
-        {/* <TextForm/> */}
-        <About />
+        <TextForm heading="Enter the text to analyze" />
+
+        {/* About.js access from Here  */}
+        {/* <About /> */}
       </div>
     </>
   );
--- REACT.js/my-app/src/Components/7. UseState & state/TextForm.js
+++ REACT.js/my-app/src/Components/7. UseState & state/TextForm.js
@@ -23,6 +23,17 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const [text, setText] = useState("");
   // text = "new text" //Wrong way to change the state
   // setText("new text"); //Correct way to change the state
@@ -43,14 +54,25 @@ export default function TextForm(props) {
             {" "}
           </textarea>
         </div>
-        <button className="btn btn-primary " onClick={handleUpClick}>
+
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
           Convert to UpperCase
         </button>
-        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
           Convert to LowerCase
         </button>
-        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
           Clear Text
+        </button>
+
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
         </button>
       </div>
       <div className="container my-2">
