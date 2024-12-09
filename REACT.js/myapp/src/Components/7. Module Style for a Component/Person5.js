47435b23116205e81892938710ef173a8b3e464e
ANISH-SINGH <anishraaz90@gmail.com>
2024-12-09 10:15:27 +0530

    module

---
+++ REACT.js/myapp/src/Components/7. Module Style for a Component/Person5.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Person5.css";
+
+class Person4 extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h1>
+          <i>Welcome To WsCube Tech</i>
+        </h1>
+        <h2>Testing</h2>
+      </div>
+    );
+  }
+}
+
+export default Person4;
