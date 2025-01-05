88af1f074485f21c26d5bdced1a31866914ba3e5
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-06 00:04:15 +0530

    node start

--- NODE.js/code1.js
+++ NODE.js/code1.js
@@ -1,8 +1,4 @@
-const http = require("http");
-
-http
-  .createServer((req, resp) => {
-    resp.write("Hello this is Anish Singh");
-    resp.end();
-  })
-  .listen(4500);
+var a = 20;
+var b = 30;
+var c = a + b;
+console.log(c);
