56946f5ecdc9f3fd044b5db4a9caca64a984da7c
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-22 23:08:21 +0530

    Fist

--- NODE/Node.js CodeEater/code1.js
+++
@@ -1,7 +0,0 @@
-const http = require("http"); //import module
-
-const server = http.createServer((req, res) => {
-  console.log("Hello World");
-});
-
-server.listen(3000);
--- NODE/Node.js CodeEater/code3.js
+++
@@ -1,7 +0,0 @@
-const http = require("http"); //import module
-
-const server = http.createServer((req, res) => {
-  console.log("URL:", req.url, "Method:", req.method, "Header:", req.headers);
-});
-
-server.listen(3000);
