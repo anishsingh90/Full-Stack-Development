38fd7804cbc525593b8d440115091cfd22108393
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-26 23:00:13 +0530

    simple react coce

--- REACT/myapp/src/App.js
+++ REACT/myapp/src/App.js
@@ -1,28 +1,9 @@
-import Student from "./component/functional component/student";
-import React from "react";
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "Anish",
-    };
-  }
-
-  render() {
-    return (
-      <>
-        <div className="App">
-          <h1>Props !</h1>
-          <Student name={this.state.name} email="anishraaz90@gmail.com" />
-
-          <button onClick={() => this.setState({ name: "Singh" })}>
-            Update Name
-          </button>
-        </div>
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
 }
 
 export default App;
