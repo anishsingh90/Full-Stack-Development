91d11aa98bff5e07bd43a44584cd9213e80ba47f
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-27 22:27:44 +0530

    Hide and Show by Toggle button

--- REACT JS/react-app/src/App.js
+++ REACT JS/react-app/src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       {status ? <h1>Hello World !</h1> : null}
 
-      <button onClick={() => setStatus(false)}>Hide</button>
-      <button onClick={() => setStatus(true)}>Show</button>
+      <button onClick={() => setStatus(!status)}>Toggle</button>
     </div>
   );
 }
