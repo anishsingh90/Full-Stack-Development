9556110133a24709f560cea11462cc0f62b2bd2b
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-27 22:47:53 +0530

    Basic Form

--- REACT JS/react-app/src/App.js
+++ REACT JS/react-app/src/App.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [status, setStatus] = React.useState(true);
+  const [name, setName] = useState("");
+  const [tnc, setTnc] = useState(false);
+  const [interest, setIntrest] = useState("");
+  function getFormData(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      {status ? <h1>Hello World !</h1> : null}
-
-      <button onClick={() => setStatus(!status)}>Toggle</button>
+      <h1>Handle Form in React</h1>
+      <br />
+      <br />
+      <form onSubmit={getFormData}>
+        <input
+          type="text"
+          placeholder="enter name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <br />
+        <select onChange={(e) => setIntrest(e.target.value)}>
+          <option>Select Options</option>
+          <option>Marvel</option>
+          <option>DC</option>
+        </select>
+        <br />
+        <br />
+        <input
+          type="checkbox"
+          onChange={(e) => setTnc(e.target.checked)}
+        />{" "}
+        <span>Accept Terms and conditions</span>
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
