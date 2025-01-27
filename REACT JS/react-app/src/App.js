4493db8a3711d795f6392ebe5ba8bf73df08c907
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-27 09:59:45 +0530

    Update Props with Class Component

--- REACT JS/react-app/src/App.js
+++ REACT JS/react-app/src/App.js
@@ -1,15 +1,31 @@
-//Props with Class Component
+//Update Props with Class Component
+import React from "react";
+
 import Student from "./Component/Student";
 
-function App() {
-  return (
-    <>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Anish",
+      email: "anishraaz90@gmail.com",
+    };
+  }
+
+  render() {
+    return (
       <div className="App">
         <h1>Props !</h1>
-        <Student name="Anish" email="anish90@gmail.com" />
+        <Student name={this.state.name} email={this.state.email} />
+        <button onClick={() => this.setState({ name: "Singh" })}>
+          Update Name
+        </button>
+        <button onClick={() => this.setState({ email: "singh90@gmail.com" })}>
+          Update Email
+        </button>
       </div>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
