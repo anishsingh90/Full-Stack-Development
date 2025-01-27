b15991740d276b9ff7e9fcc808b8b075de25b935
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-27 23:08:59 +0530

    Make Basic Server

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -1,1 +1,8 @@
-console.log(__filename);
+const http = require("http");
+
+http
+  .createServer((req, resp) => {
+    resp.write("Hello this is Anish Singh");
+    resp.end();
+  })
+  .listen(4500);
