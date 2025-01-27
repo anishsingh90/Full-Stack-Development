9b3bd21db47e0f0cd0ea360824231bba5f6f02fe
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-27 11:15:16 +0530

    access data from another file

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -1,4 +1,7 @@
+let app = require("./app");
+
 const arr = [2, 4, 7, 1, 3, 8, 3];
 
-console.log(arr);
-console.log(arr[0]);
+console.log(app);
+console.log("x: ", app.x);
+console.log("y: ", app.y);
