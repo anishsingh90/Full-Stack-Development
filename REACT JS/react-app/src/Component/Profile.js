2816e0f07ae31e7fdaf9dfb6bbf1a43204feda2e
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-28 11:51:26 +0530

    If-else Condition

--- REACT JS/react-app/src/Component/Profile.js
+++ REACT JS/react-app/src/Component/Profile.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 
 function Profile() {
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(1);
+  //1, 2, 3
 
   return (
-    // Conditional rendering in React Js
-    <div>{loggedIn ? <h1>Welcome Anish</h1> : <h1>Welcome Guest</h1>}</div>
+    // Conditional(If, Else-If, Else) rendering in React Js
+    <div>
+      {loggedIn == 1 ? (
+        <h1>Welcome User 1</h1>
+      ) : loggedIn == 2 ? (
+        <h1>Welcome User 2</h1>
+      ) : (
+        <h1>Welcome Guest</h1>
+      )}
+    </div>
   );
 }
 
