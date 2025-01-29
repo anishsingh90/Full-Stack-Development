63f3416e80010a5517caf6dd3e880b52713b1d00
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-29 17:55:41 +0530

    Input from command line-I

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -1,9 +1,1 @@
-const http = require("http");
-const data = require("./data");
-http
-  .createServer((req, resp) => {
-    resp.writeHead(200, { "Content-Type": "applicationjson" });
-    resp.write(JSON.stringify(data));
-    resp.end();
-  })
-  .listen(5000);
+console.log(process.argv);
