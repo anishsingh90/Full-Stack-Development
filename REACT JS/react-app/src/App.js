74b48be25c5bd4f6ad1e6551b3d99a19125a2520
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-29 17:30:41 +0530

    Component Did Mount

--- REACT JS/react-app/src/App.js
+++ REACT JS/react-app/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import User from "./Component/User";
 
-function App() {
-  return (
-    <div className="App">
-      <User />
-    </div>
-  );
+class App extends React.Component() {
+  constructor() {
+    super();
+    console.warn("constructor");
+  }
+  componentDidMount() {
+    console.warn("componentDidMount");
+  }
+  render() {
+    console.warn("render");
+
+    return (
+      <div className="App">
+        <h1>Component Did Mount</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
