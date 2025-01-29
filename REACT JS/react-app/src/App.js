b2e7941c1ce52d1549f596b74aad0364bf32e94e
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-29 17:14:13 +0530

    render life Cycle with State

--- REACT JS/react-app/src/App.js
+++ REACT JS/react-app/src/App.js
@@ -2,12 +2,9 @@ import React from "react";
 import User from "./Component/User";
 
 function App() {
-  const [name, setName] = React.useState("Anish");
-
   return (
     <div className="App">
-      <User name={name} />
-      <button onClick={() => setName("Singh")}>Update Name</button>
+      <User />
     </div>
   );
 }
--- REACT JS/react-app/src/Component/User.js
+++ REACT JS/react-app/src/Component/User.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 class User extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "anishraaz90@gmail.com",
+    };
+  }
   render() {
     return (
       <div>
-        <h1>User Component {this.props.name}</h1>
+        <h1>User Component</h1>
+        <p>Email: {this.state.email}</p>
+        <button
+          onClick={() => this.setState({ email: "raazanish01@gmail.com" })}
+        >
+          Update Email
+        </button>
       </div>
     );
   }
