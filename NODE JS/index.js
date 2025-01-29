0a48b077e78b545b8e36e6d108b5a296b8bff4dc
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-29 18:09:51 +0530

    Input from command line-III

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -1,3 +1,14 @@
-console.log(process.argv);
-console.log(process.argv[2]);
-console.log(process.argv[3]);
+const fs = require("fs");
+
+const input = process.argv;
+
+if (input[2] == "add") {
+  fs.writeFileSync(input[3], input[4]);
+} else if (input[2] == "remove") {
+  fs.unlinkSync(input[3]);
+} else {
+  console.log("Invalid Output");
+}
+
+//node index.js add orange.txt 'this is color and fruit'
+//node index.js remove orange.txt
