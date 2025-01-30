bc8fb077dc8313da086a3db6da2a1c7e028b8092
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-30 18:07:43 +0530

    create a file in folder

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 const dirPath = path.join(__dirname, "file");
-console.log(dirPath);
+
+for (i = 0; i < 5; i++) {
+  fs.writeFileSync(dirPath + "/hello" + i + ".txt", "a simple text file");
+  fs.writeFileSync(dirPath + `/raaz${i}.txt`, "a simple text file");
+}
