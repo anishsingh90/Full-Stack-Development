579fed654050660acbe903290b35b6435dcc810a
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-30 18:15:26 +0530

    read the file from directory

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -2,7 +2,13 @@ const fs = require("fs");
 const path = require("path");
 const dirPath = path.join(__dirname, "file");
 
-for (i = 0; i < 5; i++) {
-  fs.writeFileSync(dirPath + "/hello" + i + ".txt", "a simple text file");
-  fs.writeFileSync(dirPath + `/raaz${i}.txt`, "a simple text file");
-}
+// for (i = 0; i < 5; i++) {
+//   fs.writeFileSync(dirPath + "/hello" + i + ".txt", "a simple text file");
+//   fs.writeFileSync(dirPath + `/raaz${i}.txt`, "a simple text file");
+// }
+
+fs.readdir(dirPath, (err, files) => {
+  files.forEach((item) => {
+    console.log(item);
+  });
+});
