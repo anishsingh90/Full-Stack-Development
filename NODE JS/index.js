261447fe18f239d0036d2575e272910405344c3e
ANISH-SINGH <anishraaz90@gmail.com>
2025-01-30 17:53:08 +0530

    path of existing file in current directory

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -1,14 +1,4 @@
 const fs = require("fs");
-
-const input = process.argv;
-
-if (input[2] == "add") {
-  fs.writeFileSync(input[3], input[4]);
-} else if (input[2] == "remove") {
-  fs.unlinkSync(input[3]);
-} else {
-  console.log("Invalid Output");
-}
-
-//node index.js add orange.txt 'this is color and fruit'
-//node index.js remove orange.txt
+const path = require("path");
+const dirPath = path.join(__dirname, "file");
+console.log(dirPath);
