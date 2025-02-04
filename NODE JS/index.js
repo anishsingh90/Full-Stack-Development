b0ba9480114c18f1f6c4511c67eb90d53202b0d9
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-04 17:53:34 +0530

    Delete Operation

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -3,6 +3,6 @@ const path = require("path");
 const dirPath = path.join(__dirname, "crud");
 const filePath = `${dirPath}/apple.txt`;
 
-fs.appendFile(filePath, " and file name is apple.txt", (err) => {
-  if (!err) console.log("file is updated");
+fs.rename(filePath, `${dirPath}/fruit.txt`, () => {
+  if (!err) console.log("file name is updated");
 });
