d62328328681756abe4621b3911193ad1c85d134
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-04 17:17:31 +0530

    useEffect with specific state and props

--- REACT JS/react-app/src/App.js
+++ REACT JS/react-app/src/App.js
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState(10);
+  const [count, setCount] = useState(100);
+
+  // useEffect on data
   useEffect(() => {
-    console.log("useEffect in React");
-  });
+    console.log("useEffect Called");
+  }, [data]);
+
+  // useEffect on Count
+  useEffect(() => {
+    alert("count is " + count);
+  }, [count]);
+
   return (
     <div className="App">
-      <h1>useEffect in React {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Update Counter</button>
+      <h1>Count: {count}</h1>
+      <h1>Data: {data}</h1>
+      <button onClick={() => setCount(count + 1)}>Update Count</button>
+      <button onClick={() => setData(data + 1)}>Update Data</button>
     </div>
   );
 }
