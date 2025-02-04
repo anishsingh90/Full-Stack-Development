9d1a66615dd84b19297038c3522bb9f9b39bc914
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-04 17:48:07 +0530

    React Operation

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -2,4 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const dirPath = path.join(__dirname, "crud");
 const filePath = `${dirPath}/apple.txt`;
-fs.writeFileSync(filePath, "This is a simple text file");
+
+fs.readFile(filePath, "utf8", (err, item) => {
+  console.log(item);
+});
