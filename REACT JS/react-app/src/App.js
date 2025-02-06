546290c312761abd2315ba83f1f616e0ec0de584
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-06 17:50:16 +0530

    Handle Array with List-01

--- REACT JS/react-app/src/App.js
+++ REACT JS/react-app/src/App.js
@@ -1,14 +1,42 @@
 import React from "react";
 
 function App() {
-  const students = ["Anish", "Sidhu", "Sam", "Peter"];
+  // const students = ["Anish", "Sidhu", "Sam", "Peter"];
 
+  const students = [
+    {
+      name: "anil",
+      email: "anish90@gmail.com",
+      contact: 111,
+    },
+    {
+      name: "sidhu",
+      email: "sidhu87@gmail.com",
+      contact: 763,
+    },
+    {
+      name: "sam",
+      email: "sam54@gmail.com",
+      contact: 873,
+    },
+    {
+      name: "peter",
+      email: "peter54@gmail.com",
+      contact: 8473,
+    },
+  ];
   return (
     <div className="App">
       <h1>Handle Array with List</h1>
-      {students.map((data) => (
-        <h1>Name is : {data}</h1>
-      ))}
+      <table>
+        {students.map((data) => (
+          <tr>
+            <td>Name: {data.name}</td>
+            <td>Email: {data.email}</td>
+            <td>Contact: {data.contact}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
