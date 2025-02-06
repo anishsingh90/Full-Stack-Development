10dbeb40f83079a78a3cb3c9e8303a7e5241f608
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-06 10:17:55 +0530

    Install react-bootstrap

--- REACT JS/react-app/src/App.js
+++ REACT JS/react-app/src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
-import "./Component/style.css";
-import style from "./Component/custom.module.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="primary">Style type 1 in React js</h1>
-
-      {/* Inline CSS  */}
-      <h1 style={{ color: "red", backgroundColor: "black" }}>
-        Style type 2 in React js
-      </h1>
-
-      {/* Modular CSS  */}
-      <h1 className={style.success}>Style type 3 in React js</h1>
+      <h1>Install Bootstrap</h1>
+      <Button onClick={() => alert("Hello")}>Click Me</Button>
+      <br />
+      <br />
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="success">Success</Button>
+      <Button variant="warning">Warning</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="info">Info</Button>
+      <Button variant="light">Light</Button>
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
     </div>
   );
 }
