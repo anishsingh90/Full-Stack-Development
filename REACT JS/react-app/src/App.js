e63e37940c2d70bbd623c9ab1d50e0eda4f58c46
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-06 17:39:54 +0530

    Handle Array with list

--- REACT JS/react-app/src/App.js
+++ REACT JS/react-app/src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 
 function App() {
+  const students = ["Anish", "Sidhu", "Sam", "Peter"];
+
   return (
     <div className="App">
-      <h1>Install Bootstrap</h1>
-      <Button onClick={() => alert("Hello")}>Click Me</Button>
-      <br />
-      <br />
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="success">Success</Button>
-      <Button variant="warning">Warning</Button>
-      <Button variant="danger">Danger</Button>
-      <Button variant="info">Info</Button>
-      <Button variant="light">Light</Button>
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
+      <h1>Handle Array with List</h1>
+      {students.map((data) => (
+        <h1>Name is : {data}</h1>
+      ))}
     </div>
   );
 }
