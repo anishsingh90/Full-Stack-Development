0ac3ffdcce59d308444ebf33b6e6bf87def6c5e7
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-07 18:09:07 +0530

    Routing with express js

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Welcome, this is Home Page");
+app.get("", (req, resp) => {
+  console.log("data sent by browser", req.query.name);
+  resp.send("Welcome, this is Home Page");
 });
 
-app.get("/about", (req, res) => {
-  res.send("Welcome, this is About Page");
+app.get("/about", (req, resp) => {
+  resp.send("Welcome, this is About Page");
 });
 
-app.get("/help", (req, res) => {
-  res.send("Welcome, this is Help Page");
+app.get("/help", (req, resp) => {
+  resp.send("Welcome, this is Help Page");
 });
 
 app.listen(5000);
