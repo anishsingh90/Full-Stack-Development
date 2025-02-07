3fb42faa1ed289decd8f482631a344f0adb828eb
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-07 14:57:16 +0530

    Change props with functional component

--- PRACTICE REACT JS/react-app/src/App.js
+++ PRACTICE REACT JS/react-app/src/App.js
@@ -1,19 +1,18 @@
-import React from "react";
-import "./component/style.css";
-import style from "./component/custom.module.css";
+import React, { useState } from "react";
+import Practice from "./component/Practice";
 
 function App() {
+  const [name, setName] = useState("Anish");
   return (
     <div className="App">
-      <h1 className="primary">Style type 1 in React js</h1>
-
-      {/* Inline CSS  */}
-      <h1 style={{ color: "red", backgroundColor: "black" }}>
-        Style type 2 in React js
-      </h1>
-
-      {/* Modular CSS  */}
-      <h1 className={style.success}>Style type 3 in React js</h1>
+      <Practice name={name} />
+      <button
+        onClick={() => {
+          setName("Singh");
+        }}
+      >
+        Update Name
+      </button>
     </div>
   );
 }
--- PRACTICE REACT JS/react-app/src/component/Practice.js
+++ PRACTICE REACT JS/react-app/src/component/Practice.js
@@ -1,11 +1,7 @@
 function Practice(props) {
   return (
     <div className="Practice">
-      <h1>Practice</h1>
       <h1>{props.name}</h1>
-      <h1>{props.email}</h1>
-      <h1>{props.other.address}</h1>
-      <h1>{props.other.mobile}</h1>
     </div>
   );
 }
