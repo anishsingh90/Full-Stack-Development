537893554c42cdb7e014cbd41b4d714d4d6feee3
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-12 17:36:55 +0530

    Update Props with Class Component

--- PRACTICE REACT JS/react-app/src/App.js
+++ PRACTICE REACT JS/react-app/src/App.js
@@ -1,12 +1,30 @@
+import React from "react";
 import Practice from "./component/Practice";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Props !</h1>
-      <Practice name="Anish" email="anishraaz90@gmail.com" />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Anish",
+      email: "anishraaz90@gmail.com",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Practice name={this.state.name} email={this.state.email} />
+
+        <button onClick={() => this.setState({ name: "Singh" })}>
+          Update Name
+        </button>
+
+        <button onClick={() => this.setState({ email: "singh90@gmail.com" })}>
+          Update Email
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- PRACTICE REACT JS/react-app/src/component/Practice.js
+++ PRACTICE REACT JS/react-app/src/component/Practice.js
@@ -4,8 +4,8 @@ class Practice extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.props.name}</h1>
-        <h1>{this.props.email}</h1>
+        <h1>Name = {this.props.name}</h1>
+        <h1>Email = {this.props.email}</h1>
       </div>
     );
   }
