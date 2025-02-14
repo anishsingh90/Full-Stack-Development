77ffb4e084599a34111bd3a4b650ee2a5d295951
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-14 22:56:37 +0530

    Print input box val by button

--- PRACTICE REACT JS/react-app/src/App.js
+++ PRACTICE REACT JS/react-app/src/App.js
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 
 function App() {
   const [data, setData] = useState(null);
+  const [print, setPrint] = useState(false);
 
   function getData(val) {
     setData(val.target.value);
   }
+
   return (
     <div className="App">
       <h1>Hello</h1>
-      <h1>{data}</h1>
+      {print ? <h1>{data}</h1> : null}
       <input type="text" onChange={getData} />
+      <button onClick={() => setPrint(true)}>Print Data</button>
     </div>
   );
 }
