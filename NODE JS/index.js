4bc41386a99bd986bba3ca07c4b6b4e5f7d6f380
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-17 18:13:53 +0530

    Template Engine

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -4,7 +4,8 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "public");
 
-// app.use(express.static(publicPath));
+app.set("view engine", "ejs");
+
 app.get("/about", (req, resp) => {
   resp.sendFile(`${publicPath}/about.html`);
 });
@@ -12,4 +13,9 @@ app.get("/about", (req, resp) => {
 app.get("/index", (req, resp) => {
   resp.sendFile(`${publicPath}/index.html`);
 });
+
+app.get("/profile", (req, resp) => {
+  resp.render("profile");
+});
+
 app.listen(5000);
