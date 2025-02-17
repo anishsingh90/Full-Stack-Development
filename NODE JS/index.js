c6eaf2bff1164ff4712239edade4b64763c77831
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-17 17:54:52 +0530

    Remove extension from URL

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -4,8 +4,12 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "public");
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.get("/about", (req, resp) => {
+  resp.sendFile(`${publicPath}/about.html`);
+});
 
+app.get("/index", (req, resp) => {
+  resp.sendFile(`${publicPath}/index.html`);
+});
 app.listen(5000);
-
-//http://localhost:5000/about.html
