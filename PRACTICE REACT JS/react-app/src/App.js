f5d9e840ecab7488c6afefa6db67af477b0445b2
ANISH-SINGH <anishraaz90@gmail.com>
2025-02-18 17:19:05 +0530

    Hide and Show Toggle button

--- PRACTICE REACT JS/react-app/src/App.js
+++ PRACTICE REACT JS/react-app/src/App.js
@@ -4,10 +4,8 @@ function App() {
   const [status, setStatus] = React.useState(true);
   return (
     <div className="App">
-      {status ? <h1>Hello World !</h1> : null}
-
-      <button onClick={() => setStatus(false)}>Hide</button>
-      <button onClick={() => setStatus(true)}>Show</button>
+      {status ? <h1>Hello World Anish !</h1> : null}
+      <button onClick={() => setStatus(!status)}>Toggle</button>
     </div>
   );
 }
